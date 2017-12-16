d01191109bf0307326e7850c028e0d427943099b
Aakarsh Nair <aakarsh.nair@gmail.com>
2017-12-15 16:02:59 -0800

    Adding .gitignore.

--- hello.c
+++ hello.c
@@ -6,9 +6,11 @@ int hello_init(void)
     pr_alert("Hello World :)\n");
     return 0;
 }
+
 void hello_exit(void)
 {
     pr_alert("Goodbye World!\n");
 }
+
 module_init(hello_init);
 module_exit(hello_exit);
